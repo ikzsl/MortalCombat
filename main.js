4d9028bdce047545ab063a06cd94b19ead5b859e
ikzsl <ikzsl@yandex.ru>
2021-10-13 19:33:08 +0300

    fix adding classes)

--- main.js
+++ main.js
@@ -3,45 +3,54 @@ const player1 = {
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
   weapon: ['gun'],
-  attack: function() {
-    console.log(`${this.name} fight...` )
+  attack: function () {
+    console.log(`${this.name} fight...`);
   },
-}
+};
 
 const player2 = {
   name: 'Max',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
   weapon: ['knife'],
-  attack: function() {
-    console.log(`${this.name} fight...` )
+  attack: function () {
+    console.log(`${this.name} fight...`);
   },
-}
+};
 
 const $arenas = document.querySelector('.arenas');
 
 function createPlayer(playerClass, player) {
-  const $player = document.createElement(`div.${playerClass}`);
-  
-  const $progressbar = document.createElement('div.progressbar');
-  const $life = document.createElement('div.life');
+  const $player = document.createElement('div');
+  $player.classList.add(playerClass);
+
+  const $progressbar = document.createElement('div');
+  $progressbar.classList.add('progressbar');
+
+  const $life = document.createElement('div');
+  $life.classList.add('life');
+
   $life.style.width = '100%';
-  $life.innerText = player.hp ;
-  const $name = document.createElement(`div.name`);
+  $life.innerText = player.hp;
+
+  const $name = document.createElement('div');
+  $name.classList.add('name');
+
   $name.innerText = player.name;
   $progressbar.appendChild($life);
   $progressbar.appendChild($name);
 
-  const $character = document.createElement('div.character');
+  const $character = document.createElement('div');
+  $character.classList.add('character');
+
   const $img = document.createElement('img');
-  $img.src=player.img;
+  $img.src = player.img;
   $character.appendChild($img);
   $player.appendChild($progressbar);
   $player.appendChild($character);
-  $arenas.appendChild($player)
-  return $player
+  $arenas.appendChild($player);
+  return $player;
 }
 
 createPlayer('player1', player1);
 createPlayer('player2', player2);
-
